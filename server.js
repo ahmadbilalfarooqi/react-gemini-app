ce5456e2d283d7cbb979147001beb650605a62f4
ahmadbilal2023 <ahmadbilal20152016@gmail.com>
2024-02-26 15:27:05 +0500

    server.js file update

--- server.js
+++ server.js
@@ -9,16 +9,17 @@ app.use(express.json());
 require("dotenv").config();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEN_AI_KEY);
+const genAI = new GoogleGenerativeAI(process.env.REACT_APP_VERCEL_ENV);
 app.post("/gemini", async (req, res) => {
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
   const chat = model.startChat({
     history: req.body.history,
   });
   const msg = await req.body.message;
+
   const result = await chat.sendMessage(msg);
-  const response = result.response;
-  const text = response.text();
+  const response = await result.response;
+  const text = await response.text();
   res.send(text);
 });
 
